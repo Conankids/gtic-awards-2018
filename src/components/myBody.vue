6212780ac679b54463ae9b676c47af1bbafd8e27
jmc <935453286@qq.com>
2018-01-03 15:10:50 +0800

    同步

--- src/components/myBody.vue
+++ src/components/myBody.vue
@@ -36,10 +36,10 @@
       <div class="part-body">
         <div class="introduction-detail">
           <p>
-            GTIC AWARDS 2018，由智东西和极果权威发起。此次年度评选聚焦智能变革力量，着眼产业升级趋势，将对在2017年度有着卓越表现、快速成长、大胆创新、取得商业突破的优秀公司、产品和解决方案进行表彰。</p>
+            GTIC AWARDS 2018，由智东西联合极果和AWE权威发起。此次年度评选聚焦智能变革力量，着眼产业升级趋势，将对在2017年度有着卓越表现、快速成长、大胆创新、取得商业突破的优秀公司、产品和解决方案进行表彰。</p>
           <p><br>走过2016年的技术普及元年，人工智能在2017年与实体经济快速融合，开始对各个行业进行赋能和变革。不过，这不只是人工智能的冲锋号角，也是国内传统产业升级的大契机。那么，在这一轮智能化浪潮中，谁将是这一年度最具变革性的力量？谁又是过去一年跑的最快的AI先锋？谁的产品在突破常规，引领创新？谁的解决方案又在很好地为客户创造价值呢？
           </p>
-          <p><br>2018年3月9日，GTIC 2018全球AI芯片创新峰会将在上海召开。届时，我们将在峰会颁奖仪式上正式公布GTIC AWARDS 2018的获奖名单，并向获奖企业颁奖。<br></p>
+          <p><br>2018年3月9日，GTIC 2018全球AI芯片创新峰会将于AWE同期在上海召开。届时，我们将在峰会颁奖仪式上正式公布GTIC AWARDS 2018的获奖名单，并向获奖企业颁奖。<br></p>
         </div>
         <div class="left-line"></div>
         <div class="right-line"></div>
@@ -107,7 +107,6 @@
               <p>-获奖企业需要出席GTIC AWARDS 2018颁奖仪式。</p>
             </div>
             <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-            <!--<a class="apply-btn closed" href="javascript:;" target="_blank">敬请期待</a>-->
             <a class="apply-btn" href="javascript:;" target="_blank">敬请期待</a>
           </li>
           <li>
@@ -132,7 +131,7 @@
               <p>-最终评选出10家获奖公司。</p>
             </div>
             <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-            <a class="apply-btn" href="http://cn.mikecrm.com/Yc0hq7M" target="_blank">报名</a>
+            <a class="apply-btn" _href="http://cn.mikecrm.com/Yc0hq7M" target="_blank">{{'敬请期待'||'报名'}}</a>
           </li>
           <li>
             <div class="shadow">
@@ -144,103 +143,18 @@
                 <p>Innovative Product</p>
               </div>
             </div>
-            <div class="part-body">
-                <ul class="awards-list">
-                    <li>
-                        <div class="shadow">
-                            <div class="awards-img">
-                                <img src="../assets/images/awards1.png" alt="">
-                            </div>
-                            <div class="awards-name">
-                                <h3>年度变革力量</h3>
-                                <p>Global Revolution</p>
-                            </div>
-                        </div>
-                        <div class="awards-detail">
-                            <p>“GTIC AWARDS年度大奖，为在2017年度最具变革力、有巨大影响力的公司授予最高荣誉。”</p>
-                            <p><br>奖项介绍</p>
-                            <p>-系企业奖项，面向独角兽企业、上市企业、大型公司（未上市）；</p>
-                            <p>-将由主办方进行综合评估，最终评选出1家获奖企业；</p>
-                            <p>-获奖企业需要出席GTIC AWARDS 2018颁奖仪式。</p>
-                        </div>
-                        <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-                        <a class="apply-btn closed" href="javascript:;" target="_blank">敬请期待</a>
-                    </li>
-                    <li>
-                        <div class="shadow">
-                            <div class="awards-img">
-                                <img src="../assets/images/awards2.png" alt="">
-                            </div>
-                            <div class="awards-name">
-                                <h3>年度AI先锋</h3>
-                                <p>AI Pioneer</p>
-                            </div>
-                        </div>
-                        <div class="awards-detail">
-                            <p>“不要轻视任何一家创业公司。今天的先锋，或许未来就是一只独角兽。”</p>
-                            <p><br>参选条件</p>
-                            <p>-参选公司为最近5年创立的人工智能创业公司；</p>
-                            <p>-参选公司2017年度在技术创新、业务增长、营收增长、融资等任一方面表现亮眼；</p>
-                            <p>-获奖公司需要出席GTIC AWARDS 2018颁奖仪式。</p>
-                            <p><br>评选标准</p>
-                            <p>-将评估参选公司在2017年的创新能力、发展速度、影响力和未来价值；</p>
-                            <p>-大众投票权重占比30%，评委嘉宾评分权重占70%；</p>
-                            <p>-最终评选出10家获奖公司。</p>
-                        </div>
-                        <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-                        <a class="apply-btn" href="http://cn.mikecrm.com/Yc0hq7M" target="_blank">报名</a>
-                    </li>
-                    <li>
-                        <div class="shadow">
-                            <div class="awards-img">
-                                <img src="../assets/images/awards3.png" alt="">
-                            </div>
-                            <div class="awards-name">
-                                <h3>年度创新产品</h3>
-                                <p>Innovative Product</p>
-                            </div>
-                        </div>
-                        <div class="awards-detail">
-                            <p>“唯有创新，才具有长久的生命力。打破常规，敢于创新，当成为本能，砥砺前行。”</p>
-                            <p><br>参选条件</p>
-                            <p>-参选产品必须是2017年发布、上市或有重大更新；</p>
-                            <p>-参选产品必须是服务于最终消费者；</p>
-                            <p><br>评选标准</p>
-                            <p>-将评估参选产品的创新性、行业影响力；</p>
-                            <p>-大众投票权重占比30%，评委嘉宾评分权重占70%；</p>
-                            <p>-最终评选出10个获奖产品。</p>
-                        </div>
-                        <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-                        <a class="apply-btn" href="http://cn.mikecrm.com/ebP5noq" target="_blank">报名</a>
-                    </li>
-                    <li>
-                        <div class="shadow">
-                            <div class="awards-img">
-                                <img src="../assets/images/awards4.png" alt="">
-                            </div>
-                            <div class="awards-name">
-                                <h3>优秀解决方案</h3>
-                                <p>Best Solution</p>
-                            </div>
-                        </div>
-                        <div class="awards-detail">
-                            <p>“把客户需求落地，创造客户认可的价值，是他们一直在追求的目标。”</p>
-                            <p><br>参选条件</p>
-                            <p>-参选方为最近3年发布或上市的行业解决方案、企业级服务或产品；</p>
-                            <p>-参选方的服务对象为企业，而非终端消费者；</p>
-                            <p>-参选方2017年度在客户价值、创新性、增长速度等任一方面表现亮眼。</p>
-                            <p><br>评选标准</p>
-                            <p>-将评估参选方的创新性、客户价值、成长性、影响力；</p>
-                            <p>-大众投票权重占比30%，评委嘉宾评分权重占70%；</p>
-                            <p>-最终评选出5个获奖者。</p>
-                        </div>
-                        <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-                        <a class="apply-btn" href="http://cn.mikecrm.com/dKMOXCe" target="_blank">报名</a>
-                    </li>
-                </ul>
+            <div class="awards-detail">
+              <p>“唯有创新，才具有长久的生命力。打破常规，敢于创新，当成为本能，砥砺前行。”</p>
+              <p><br>参选条件</p>
+              <p>-参选产品必须是2017年发布、上市或有重大更新；</p>
+              <p>-参选产品必须是服务于最终消费者；</p>
+              <p><br>评选标准</p>
+              <p>-将评估参选产品的创新性、行业影响力；</p>
+              <p>-大众投票权重占比30%，评委嘉宾评分权重占70%；</p>
+              <p>-最终评选出10个获奖产品。</p>
             </div>
             <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-            <a class="apply-btn" href="http://cn.mikecrm.com/ebP5noq" target="_blank">报名</a>
+            <a class="apply-btn" _href="http://cn.mikecrm.com/ebP5noq" target="_blank">{{'敬请期待'||'报名'}}</a>
           </li>
           <li>
             <div class="shadow">
@@ -264,7 +178,7 @@
               <p>-最终评选出5个获奖者。</p>
             </div>
             <a class="icon-add" href="javascript:;"><img src="../assets/images/icon-add.png" alt=""></a>
-            <a class="apply-btn" href="http://cn.mikecrm.com/dKMOXCe" target="_blank">报名</a>
+            <a class="apply-btn" _href="http://cn.mikecrm.com/dKMOXCe" target="_blank">{{'敬请期待'||'报名'}}</a>
           </li>
         </ul>
       </div>
@@ -303,11 +217,14 @@
         <p>/ 主办 /</p>
       </div>
       <div class="part-body">
-        <div class="logo-item">
+        <div class="logo-item" style="height: 105px">
           <img src="../assets/images/zhidx.png" alt="">
         </div>
         <div class="logo-item">
           <img src="../assets/images/jiguo.png" alt="">
+        </div>
+        <div class="logo-item">
+          <img src="../assets/images/AWE.png" alt="">
         </div>
       </div>
     </div>
